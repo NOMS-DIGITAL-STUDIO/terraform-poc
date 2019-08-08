672fd8c3100369b187221ccd16044f92df2ae32e
bell-pa <peter.bell@digital.justice.gov.uk>
2019-08-08 15:26:34 +0100

    LIC-1016 Copy in the Clearing House mail box to the RO email notifications.

--- licences/stage/vars.tf
+++ licences/stage/vars.tf
@@ -49,7 +49,7 @@ locals {
   remindersScheduleRo = "0 1 * * 1-5"
   scheduledJobAuto    = "no"
   scheduledJobOverlap = "5000"
-  notifyActiveTemplates = "CA_RETURN,CA_DECISION,RO_NEW,RO_TWO_DAYS,RO_DUE,RO_OVERDUE,DM_NEW"
+  notifyActiveTemplates = "CA_RETURN,CA_DECISION,RO_NEW,RO_TWO_DAYS,RO_DUE,RO_OVERDUE,DM_NEW,DM_TO_CA_RETURN"
   roServiceType       = "DELIUS"
   deliusApiUrl        = "https://community-api-t2.hmpps.dsd.io/communityapi/api"
 }
