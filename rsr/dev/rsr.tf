a6bafda0fd32c156a2845f0a228c94d406f305de
Ynyr Llewelyn Williams <ynyr.williams@digital.justice.gov.uk>
2021-03-02 09:21:19 +0000

    DSO-808 - removed potential security vulnerabilities and old azure templates

--- rsr/dev/rsr.tf
+++ rsr/dev/rsr.tf
@@ -27,17 +27,3 @@ module "app_service" {
   ]
   tags = var.tags
 }
-
-resource "github_repository_webhook" "rsr-deploy" {
-  repository = "rsr-calculator-service"
-  configuration {
-    #hardcoded if this is ever rebuilt will need to be updated
-    url          = "https://$rsr-dev:xfyC59uSQz6pakG3981G86u261Bh7yiuHLmYaqndksjPtGGDuDbeLeQNcLGa@rsr-dev.scm.azurewebsites.net/deploy?scmType=GitHub"
-    content_type = "form"
-    insecure_ssl = false
-  }
-
-  active = true
-
-  events = ["push"]
-}
