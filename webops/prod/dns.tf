6a2d4e1a070c5a77a5bb4f550a3745c54f495704
James Borwick <james.borwick@icloud.com>
2021-03-31 13:19:01 +0100

    Added some more DNS records

--- webops/prod/dns.tf
+++ webops/prod/dns.tf
@@ -8,7 +8,7 @@ resource "azurerm_dns_ns_record" "nomis-api" {
   name                = "nomis-api"
   zone_name           = azurerm_dns_zone.service-hmpps.name
   resource_group_name = azurerm_resource_group.group.name
-  ttl                 = "300"
+  ttl                 = 300
 
   records = ["ns2-07.azure-dns.net.", "ns3-07.azure-dns.org.", "ns1-07.azure-dns.com.", "ns4-07.azure-dns.info."]
 
@@ -44,4 +44,76 @@ resource "azurerm_dns_ns_record" "studio-hosting" {
     Service     = "WebOps"
     Environment = "Management"
   }
+}
+
+resource "azurerm_dns_a_record" "reporting_lsast_nomis" {
+  name                = "reporting.lsast-nomis"
+  zone_name           = azurerm_dns_zone.az_justice_gov_uk.name
+  resource_group_name = azurerm_resource_group.group.name
+  records             = ["10.40.44.198"]
+  ttl                 = 300
+}
+
+resource "azurerm_dns_cname_record" "bridge_oasys" {
+  name                = "bridge-oasys"
+  zone_name           = azurerm_dns_zone.az_justice_gov_uk.name
+  resource_group_name = azurerm_resource_group.group.name
+  record              = "oasys-prod.uksouth.cloudapp.azure.com"
+  ttl                 = 300
+}
+
+resource "azurerm_dns_cname_record" "bridge_onr" {
+  name                = "bridge-onr"
+  zone_name           = azurerm_dns_zone.az_justice_gov_uk.name
+  resource_group_name = azurerm_resource_group.group.name
+  record              = "onr-prod.uksouth.cloudapp.azure.com"
+  ttl                 = 300
+}
+
+resource "azurerm_dns_cname_record" "bridge_pp_oasys" {
+  name                = "bridge-pp-oasys"
+  zone_name           = azurerm_dns_zone.az_justice_gov_uk.name
+  resource_group_name = azurerm_resource_group.group.name
+  record              = "oasys-preprod.uksouth.cloudapp.azure.com"
+  ttl                 = 300
+}
+
+resource "azurerm_dns_cname_record" "bridge_pp_onr" {
+  name                = "bridge-pp-onr"
+  zone_name           = azurerm_dns_zone.az_justice_gov_uk.name
+  resource_group_name = azurerm_resource_group.group.name
+  record              = "onr-preprod.uksouth.cloudapp.azure.com"
+  ttl                 = 300
+}
+
+resource "azurerm_dns_cname_record" "bridge_practice_oasys" {
+  name                = "bridge-practice.oasys"
+  zone_name           = azurerm_dns_zone.az_justice_gov_uk.name
+  resource_group_name = azurerm_resource_group.group.name
+  record              = "oasys-prod.uksouth.cloudapp.azure.com"
+  ttl                 = 3600
+}
+
+resource "azurerm_dns_cname_record" "bridge_training_oasys" {
+  name                = "bridge-training.oasys"
+  zone_name           = azurerm_dns_zone.az_justice_gov_uk.name
+  resource_group_name = azurerm_resource_group.group.name
+  record              = "oasys-prod.uksouth.cloudapp.azure.com"
+  ttl                 = 3600
+}
+
+resource "azurerm_dns_cname_record" "practice_oasys" {
+  name                = "practice.oasys"
+  zone_name           = azurerm_dns_zone.az_justice_gov_uk.name
+  resource_group_name = azurerm_resource_group.group.name
+  record              = "oasys-prod.uksouth.cloudapp.azure.com"
+  ttl                 = 300
+}
+
+resource "azurerm_dns_cname_record" "training_oasys" {
+  name                = "training.oasys"
+  zone_name           = azurerm_dns_zone.az_justice_gov_uk.name
+  resource_group_name = azurerm_resource_group.group.name
+  record              = "oasys-prod.uksouth.cloudapp.azure.com"
+  ttl                 = 300
 }
