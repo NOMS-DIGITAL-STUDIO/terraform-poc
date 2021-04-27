ab7da70e0055aa94a91309c2c4afd69c76a21b48
James Borwick <james.borwick@icloud.com>
2021-04-27 10:51:00 +0100

    Added provider for APVS Production

--- webops/prod/versions.tf
+++ webops/prod/versions.tf
@@ -14,6 +14,13 @@ terraform {
 }
 provider "azurerm" {
   tenant_id       = "747381f4-e81f-4a43-bf68-ced6a1e14edf"
-  subscription_id = "a5ddf257-3b21-4ba9-a28c-ab30f751b383"
+  subscription_id = "a5ddf257-3b21-4ba9-a28c-ab30f751b383" # NOMS Digital Studio Production 1
+  features {}
+}
+
+provider "azurerm" {
+  alias           = "apvs"
+  tenant_id       = "747381f4-e81f-4a43-bf68-ced6a1e14edf"
+  subscription_id = "19c0e044-6167-4048-a817-7ac7b4323f06" # APVS Production
   features {}
 }
