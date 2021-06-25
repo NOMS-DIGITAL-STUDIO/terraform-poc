3180b9dc24af78bdab59f0f19d321a99a9f54641
wullub <william@automationlogic.com>
2021-06-25 10:49:16 +0100

    rsr set versions

--- rsr/prod/versions.tf
+++ rsr/prod/versions.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.46.1"
+      version = "=2.55.0"
     }
     github = {
       source = "integrations/github"
     }
   }
-  required_version = "= 0.14.11"
+  required_version = "= 0.15.5"
 }
 provider "azurerm" {
   tenant_id       = "747381f4-e81f-4a43-bf68-ced6a1e14edf"
